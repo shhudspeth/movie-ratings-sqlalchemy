3a2995eabf1373daec813e5c54914f98ebe5cc3e
Sarah Hudspeth <shhudspeth@gmail.com>
2020-08-07 01:03:34 +0000

    completed flask routes

--- crud.py
+++ crud.py
@@ -15,15 +15,6 @@ def create_user(email, password):
 
     return user
 
-def create_movie(title, overview, release_date, poster_path):
-    """ Create and return a new movie"""
-    movie = Movie(title=title, overview=overview, 
-                  release_date=release_date, poster_path=poster_path)
-    
-    db.session.add(movie)
-    db.session.commit()
-
-    return movie
 
 def create_movie(title, overview, release_date, poster_path):
     """ Create and return a new movie"""
@@ -36,6 +27,16 @@ def create_movie(title, overview, release_date, poster_path):
     db.session.commit()
 
     return movie
+
+
+def return_all_movies():
+    """ Create and return a list of all movie"""
+    return Movie.query.all()  
+
+
+def get_movie_by_id(movie_id):
+    """Return a movie by id"""
+    return Movie.query.get(movie_id)
 
 
 def create_rating(user, movie, score):
--- server.py
+++ server.py
@@ -1,12 +1,41 @@
 """Server for movie ratings app."""
 
-from flask import Flask
+from flask import (Flask, render_template, request, flash, session,
+                   redirect)
+from model import connect_to_db
+import crud
+
+from jinja2 import StrictUndefined
 
 app = Flask(__name__)
+app.secret_key = "dev"
+app.jinja_env.undefined = StrictUndefined
 
 
 # Replace this with routes and view functions!
+@app.route('/')
+def homepage():
+    """ View homepage """
+    return render_template('homepage.html')
+
+@app.route('/movies')
+def view_all_movies():
+    """ View a list of all movie titles """
+
+    all_movies = crud.return_all_movies()
+
+    return render_template('all_movies.html', all_movies=all_movies)
+
+@app.route('/movies/<movie_id>')
+def show_movie_detail(movie_id):
+    """Show details on a particular movie."""
+
+    movie = crud.get_movie_by_id(movie_id)
+    print(movie)
+
+    return render_template('movie_details.html', movie=movie)
 
 
 if __name__ == '__main__':
+    connect_to_db(app)
     app.run(host='0.0.0.0', debug=True)
